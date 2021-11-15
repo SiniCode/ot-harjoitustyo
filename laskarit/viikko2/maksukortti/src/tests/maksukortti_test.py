66a8730771ef24d3603e4f553443d0fcd3fda3da
Sini Arkko <sini.arkko@gmail.com>
2021-11-15 19:52:54 +0200

    loput testit luotu

--- laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
+++ laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
@@ -30,3 +30,27 @@ class TestMaksukortti(unittest.TestCase):
     def test_kortin_saldo_ei_ylita_maksimiarvoa(self):
         self.kortti.lataa_rahaa(200)
         self.assertEqual(str(self.kortti), "Kortilla on rahaa 150 euroa")
+
+    def test_syo_maukkaasti_ei_vie_saldoa_negatiiviseksi(self):
+        self.kortti.syo_maukkaasti()
+	self.kortti.syo_maukkaasti()
+	self.kortti.syo_maukkaasti()
+	self.assertEqual(str(self.kortti), "Kortilla on rahaa 2 euroa")
+
+    def test_negatiivisen_summan_lataaminen_ei_muuta_saldoa(self):
+        self.lataa_rahaa(-3)
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 10 euroa")
+
+    def test_voi_syoda_edullisesti_kun_saldo_tasan_edullisen_hinta(self):
+        self.kortti.syo_edullisesti()
+	self.kortti.syo_edullisesti()
+	self.kortti.syo_edullisesti()
+	self.kortti.syo_edullisesti()
+	self.assertEqual(str(self.kortti), "Kortilla on rahaa 0 euroa")
+
+    def test_voi_syoda_maukkaasti_kun_saldo_tasan_maukkaan_hinta(self):
+	self.kortti.lataa_rahaa(2)
+	self.kortti.syo_maukkaasti()
+	self.kortti.syo_maukkaasti()
+	self.kortti.syo_maukkaasti()
+	self.assertEqual(str(self.kortti), "Kortilla on rahaa 0 euroa")
