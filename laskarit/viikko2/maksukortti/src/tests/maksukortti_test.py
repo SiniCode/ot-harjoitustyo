5190e2fd7fd40ee9b84cb3aed153b18ce05f2100
Sini Arkko <sini.arkko@gmail.com>
2021-11-15 19:19:13 +0200

    testit korjattu uudestaan

--- laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
+++ laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
@@ -6,19 +6,19 @@ class TestMaksukortti(unittest.TestCase):
         self.kortti = Maksukortti(10)
 
     def test_konstruktori_asettaa_saldon_oikein(self):
-        self.assertEqual(str(kortti), "Kortilla on rahaa 10 euroa")
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 10 euroa")
 
     def test_syo_edullisesti_vahentaa_saldoa_oikein(self):
-        kortti.syo_edullisesti()
-        self.assertEqual(str(kortti), "Kortilla on rahaa 7.5 euroa")
+        self.kortti.syo_edullisesti()
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 7.5 euroa")
 
     def test_syo_maukkaasti_vahentaa_saldoa_oikein(self):
-        kortti.syo_maukkaasti()
-        self.assertEqual(str(kortti), "Kortilla on rahaa 6 euroa")
+        self.kortti.syo_maukkaasti()
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 6 euroa")
 
     def test_syo_edullisesti_ei_vie_saldoa_negatiiviseksi(self):
-        kortti.syo_maukkaasti()
-        kortti.syo_maukkaasti()
+        self.kortti.syo_maukkaasti()
+        self.kortti.syo_maukkaasti()
         # nyt kortin saldo on 2
-        kortti.syo_edullisesti()
-        self.assertEqual(str(kortti), "Kortilla on rahaa 2 euroa")
+        self.kortti.syo_edullisesti()
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 2 euroa")
