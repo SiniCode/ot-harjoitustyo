a8734ab5795bd029ca9135b3624678afcc0059d8
Sini Arkko <sini.arkko@gmail.com>
2021-11-23 19:25:21 +0200

    update user_repository

--- src/repositories/user_repository.py
+++ src/repositories/user_repository.py
@@ -1,6 +1,9 @@
 from entities.user import User
 from database_connection import get_db_connection
 
+def get_user_by_row(row):
+    return User(row['username'], row['password']) if row else None
+
 class UserRepository:
     """Luokka, joka vastaa käyttäjiin liittyvistä tietokantaoperaatioista."""
 
@@ -25,12 +28,30 @@ class UserRepository:
 
         cursor = self._connection.cursor()
 
-        cursor.execute('INSERT INTO Users (username, password) values (?, ?)', (user.username, user.password))
+        cursor.execute('INSERT INTO Users (username, password) values (?, ?)', [user.username, user.password])
 
         self._connection.commit()
 
         return user
 
+    def find_by_username(self, username):
+        """Palauttaa käyttäjän käyttäjätunnuksen perusteella, jos käyttäjä on tallennettu tietokantaan.
+
+        Args:
+            username: Merkkijono, joka kuvaa haettavan käyttäjän käyttäjätunnusta
+
+        Returns:
+            User-olio, joka kuvaa käyttäjätunnuksen haltijaa.
+            None, jos käyttäjätunnusta ei löydy tietokannasta.
+        """
+
+        cursor = self._connection.cursor()
+
+        cursor.execute('SELECT * FROM Users WHERE username = ?', [username])
+
+        row = cursor.fetchone()
+
+        return get_user_by_row()
 
 user_repository = UserRepository(get_db_connection())
 
