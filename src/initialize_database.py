7a098974fdb9a16781544cf7d40fcc32708093ee
Sini Arkko <sini.arkko@gmail.com>
2021-11-23 18:11:31 +0200

    initialize database

---
+++ src/initialize_database.py
@@ -0,0 +1,31 @@
+from database_connection import get_db_connection
+
+def drop_tables(connection):
+    cursor = connection.cursor()
+
+    cursor.execute('''
+        drop table if exits Users;
+    ''')
+
+    connection.commit()
+
+def create_tables(connection):
+    cursor = connection.cursor()
+
+    cursor.execute('''
+        CREATE TABLE Users (
+            id INTEGER PRIMARY KEY,
+            username TEXT,
+            password TEXT);
+    ''')
+
+    connection.commit()
+
+def initialize_database():
+    connection = get_db_connection()
+
+    drop_tables(connection)
+    create_tables(connection)
+
+if __name__ == "__main__":
+    initialize_database() 
