647943ec624315bb50626e3f5b1adb5c75117b85
Sini Arkko <sini.arkko@gmail.com>
2021-12-07 16:59:32 +0200

    add new methods

--- src/ui/ui.py
+++ src/ui/ui.py
@@ -13,9 +13,9 @@ class UI:
 
         print()
         print("Options")
-        print("  1: log in")
-        print("  2: create new user")
-        print("  3: exit")
+        print("  1: Log in")
+        print("  2: Create new user")
+        print("  3: Exit")
         print()
 
     def logged_in_options(self):
@@ -23,12 +23,11 @@ class UI:
 
         print()
         print("Options")
-        print("  1: add new recipe")
-        print("  2: find all recipes")
-        print("  3: find a recipe")
-        print("  4: rename a recipe")
-        print("  5: delete a recipe")
-        print("  6: log out")
+        print("  1: Add new recipe")
+        print("  2: Look for recipes")
+        print("  3: Update recipe")
+        print("  4: Delete recipe")
+        print("  5: Log out")
         print()
 
     def login(self):
@@ -124,38 +123,184 @@ class UI:
 
         recipes = service.find_recipes()
 
-        print("Saved recipes:")
+        print("Your recipes:")
         print()
         for r in recipes:
             print(r)
 
         self.logged_in_main()
 
+
+    def find_recipes_by_ingredient(self):
+        """Luokan metodi, joka tulostaa aakkosjärjestyksessä niiden käyttäjän tallentamien reseptien nimet, joissa annettu aines esiintyy."""
+
+        ingredient = input("Ingredient: ")
+        print()
+
+        recipes = service.find_recipes_by_ingredient(ingredient)
+
+        print(f"Your recipes including {ingredient}:")
+        print()
+        for r in recipes:
+            print(r)
+
+        self.logged_in_main()
+
+
     def find_ingredients(self):
         """Luokan metodi, joka tulostaa annetun reseptin ainekset ja niiden määrät tallennusjärjestyksessä."""
 
         name = input("Which recipe would you like to see? ")
+
         ingredients = service.find_ingredients(name)
+
         print()
         print(f"The ingredients of {name}:")
         print()
+
         for i in ingredients:
             print(f"{i[0]:40} {i[1]}")
 
         self.logged_in_main()
 
-    def rename_recipe(self):
-        """Luokan metodi, jonka avulla käyttäjä voi nimetä tallentamansa reseptin uudelleen."""
+    def look_for_recipes(self):
+        """Luokan metodi, jonka avulla käyttäjä voi tehdä hakuja tietokannasta"""
 
-        recipe = input("Which recipe would you like to rename? ")
-        print()
+        while True:
+           print()
+           print("Options:")
+           print("  1: Find all recipes")
+           print("  2: Search recipes by ingredient")
+           print("  3: Find the ingredients of a recipe")
+           print()
+
+           option = input("What would you like to do? ")
+
+           try:
+                op = int(option)
+            except ValueError:
+                continue
+
+            print()
+
+            if op == 1:
+                self.find_all_recipes()
+
+            elif op == 2:
+                self.find_recipes_by_ingredient()
+
+            elif op == 3:
+                self.find_ingredients()
+
+            else:
+                continue
+
+
+    def rename_recipe(self, recipe):
+        """Luokan metodi, jonka avulla käyttäjä voi nimetä tallentamansa reseptin uudelleen.
+
+        Args:
+            recipe: merkkijono, joka kertoo, mitä reseptiä halutaan muuttaa
+        """
+
         new_name = input("New name: ")
+
         service.change_recipe_name(recipe, new_name)
+
         print()
         print(f"Recipe renamed as {new_name}!")
 
         self.logged_in_main()
 
+    def add_an_ingredient(self, recipe):
+        """Luokan metodi, joka lisää reseptiin uuden ainesosan.
+
+        Args:
+            recipe: merkkijono, joka kertoo, mihin reseptiin ainesosa lisätään
+        """
+
+        ingredient = input("Which ingredient would you like to add? ")
+        amount = input("Amount: ")
+
+        service.insert_an_ingredient(recipe, ingredient, amount)
+
+        print()
+        print("New ingredient added!")
+
+        self.logged_in_main()
+
+    def remove_an_ingredient(self, recipe):
+        """Luokan metodi, joka poistaa reseptistä tallennetun aineksen.
+
+        Args:
+            recipe: merkkijono, joka kertoo, mistä reseptistä aines poistetaan
+        """
+
+        ingredient = input("Which ingredient would you like to remove? ")
+
+        service.delete_an_ingredient(recipe, ingredient)
+
+        print()
+        print(f"{ingredient} removed!")
+
+        self.logged_in_main()
+
+    def change_ingredient_amount(self, recipe):
+        """Luokan metodi, jonka avulla käyttäjä voi muuttaa tietyn aineksen määrää tallennetussa reseptissä.
+
+        Args:
+            recipe: merkkijono, joka kertoo, mitä reseptiä halutaan muuttaa
+        """
+
+        ingredient = input("Name of the ingredient: ")
+        amount = input("Updated amount: ")
+
+        service.change_ingredient_amount(recipe, ingredient, amount)
+
+        print()
+        print("The recipe is updated!")
+
+        self.logged_in_main()
+
+
+
+    def update_recipe(self):
+        """Luokan metodi, jonka avulla käyttäjä voi muuttaa tallentamiaan reseptejä."""
+
+        recipe = input("Which recipe would you like to update? ")
+
+        while True:
+            print()
+            print("Options: ")
+            print("  1: Rename the recipe")
+            print("  2: Add an ingredient")
+            print("  3: Remove an ingredient")
+            print("  4: Change the amount of an ingredient")
+            print()
+
+            option = input("What would you like to do? ")
+
+            try:
+                op = int(option)
+            except ValueError:
+                continue
+
+            print()
+
+            if op == 1:
+                self.rename_recipe(recipe)
+
+            elif op == 2:
+                self.add_an_ingredient(recipe)
+
+            elif op == 3:
+                self.remove_an_ingredient(recipe)
+
+            elif op == 4:
+                self.change_ingredient_amount(recipe)
+
+            else:
+                continue
 
     def delete_recipe(self):
         recipe = input("Which recipe would you like to delete? ")
@@ -224,18 +369,15 @@ class UI:
             self.add_recipe()
 
         elif op == 2:
-            self.find_all_recipes()
+            self.look_for_recipes()
 
         elif op == 3:
-            self.find_ingredients()
+            self.update_recipe()
 
         elif op == 4:
-            self.rename_recipe()
-
-        elif op == 5:
             self.delete_recipe()
 
-        elif op == 6:
+        elif op == 5:
             self.logout()
 
         else:
